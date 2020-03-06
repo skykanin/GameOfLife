294ff2838ce117bc46c8486ab0a074c43f862ada
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-06 04:47:29 +0100

    Add more game logic and change board type

--- src/GameOfLife/Game.hs
+++ src/GameOfLife/Game.hs
@@ -1,15 +1,50 @@
 module Game where
 
 import Control.Applicative (liftA2)
-import Data.Vector (Vector, iterateN)
+import Data.Vector (Vector, (!?), iterateN)
 
-type Board = Vector (Float, Float, Bool)
+data Board =
+  Board
+    { _board :: Vector Cell
+    , size :: Int
+    }
+  deriving (Show)
+
+type Cell = (Float, Float, Bool)
+
+boardSize :: Num a => a
+boardSize = 20
 
 initialGame :: Board
-initialGame = liftA2
-  (\x y -> (x, y, False))
-  (iterateN 20 (+1) 0)
-  (iterateN 20 (\x -> x - 1) 0)
+initialGame =
+  Board
+    (liftA2
+       (\x y -> (x, y, False))
+       (iterateN boardSize (+ 1) 0)
+       (iterateN boardSize (\n -> n - 1) 0))
+    boardSize
+
+findIndex :: Int -> Int -> Int -> Int
+findIndex width x y = x + width * y
+
+neighbours :: [(Float -> Float, Float -> Float)]
+neighbours =
+  [ (succ, id)
+  , (succ, succ)
+  , (succ, pred)
+  , (id, succ)
+  , (id, pred)
+  , (pred, id)
+  , (pred, succ)
+  , (pred, pred)
+  ]
+
+findNeighbours :: Board -> (Int, Int) -> Maybe (Vector Cell)
+findNeighbours (Board board w) (x, y) = do
+  let index = findIndex w x y
+  (cx, cy, _) <- board !? index
+  let indecies = map (\(f, g) -> (f cx, g cy)) neighbours
+  Nothing
 
 step :: Board -> Board
 step = undefined
--- src/GameOfLife/Rendering.hs
+++ src/GameOfLife/Rendering.hs
@@ -1,8 +1,8 @@
 module Rendering where
 
-import Graphics.Gloss
-import Game (Board)
 import Data.Vector (toList)
+import Game (Board(..))
+import Graphics.Gloss
 
 squareSize :: Num a => a
 squareSize = 25
@@ -11,16 +11,18 @@ start :: (Float, Float)
 start = (-640 / 2, 500 / 2 - squareSize)
 
 gameAsPicture :: Board -> Picture
-gameAsPicture vec = Pictures
-  [ if b
-    then Color black sq
-    else Color red sq
-  | (x, y, b) <- toList vec
-  , let xv = fst start + (x * 30)
-        yv = snd start + (y * 30)
-        sq = square xv yv
-  ]
+gameAsPicture (Board vec _) =
+  Pictures
+    [ if b
+      then Color black sq
+      else Color red sq
+    | (x, y, b) <- toList vec
+    , let xv = fst start + (x * 30)
+          yv = snd start + (y * 30)
+          sq = square xv yv
+    ]
 
 square :: Float -> Float -> Picture
-square x y = Polygon [ (x,y) , (x+s, y) , (x+s, y+s), (x, y+s) ]
-  where s = squareSize
+square x y = Polygon [(x, y), (x + s, y), (x + s, y + s), (x, y + s)]
+  where
+    s = squareSize

085593ccb1197ff61835d6eda9dd60ba70e77658
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-12 21:44:29 +0100

    Add test board for shapes

--- src/GameOfLife/Game.hs
+++ src/GameOfLife/Game.hs
@@ -1,7 +1,8 @@
 module Game where
 
 import Control.Applicative (liftA2)
-import Data.Vector (Vector, (!?), fromList, iterateN, mapMaybe)
+import Data.Vector (Vector, (!?), fromList, iterateN, length, mapMaybe)
+import Prelude hiding (length)
 
 data Board =
   Board
@@ -23,6 +24,17 @@ initialGame =
        (iterateN boardSize (+ 1) 0)
        (iterateN boardSize (\n -> n - 1) 0))
     boardSize
+
+testBoard :: Board
+testBoard = Board {_board = fromList board, size = 20}
+  where
+    board = [shapeMapper (x, y, False) | x <- [1 .. 20], y <- [1 .. 20]]
+
+shapeMapper :: Cell -> Cell
+shapeMapper (2, 3, False) = (2, 3, True)
+shapeMapper (3, 3, False) = (3, 3, True)
+shapeMapper (4, 3, False) = (4, 3, True)
+shapeMapper c = c
 
 findIndex :: Num a => a -> a -> a -> a
 findIndex width x y = x + width * y
@@ -49,5 +61,25 @@ findNeighbours (Board board w) (x, y) =
     neighboursToIndices (cx, cy, _) =
       (\(f, g) -> findIndex w (f cx) (g cy)) <$> neighbours
 
+-- Game rules
+-- Any live cell with two or three neighbors survives
+-- Any dead cell with three live neighbors becomes a live cell.
+-- All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+applyRules :: Board -> Cell -> Maybe Cell
+applyRules board c@(x, y, True) =
+  (\v ->
+     if length v == 2 || length v == 3
+       then c
+       else (x, y, False)) <$>
+  findNeighbours board (x, y)
+applyRules board c@(x, y, False) =
+  (\v ->
+     if length v == 3
+       then (x, y, True)
+       else c) <$>
+  findNeighbours board (x, y)
+
+-- If indexing of cells out of bounds return initial game board
 step :: Board -> Board
-step b = b
+step board@(Board b s) =
+  maybe initialGame (`Board` s) (traverse (applyRules board) b)
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Game (initialGame, step)
+import Game (initialGame, step, testBoard)
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
 import Rendering (gameAsPicture)
@@ -8,8 +8,8 @@ import Rendering (gameAsPicture)
 backgroundColour :: Color
 backgroundColour = makeColor 255 255 255 255
 
-window = InWindow "GameOfLife" (640, 500) (100, 100)
+window = InWindow "GameOfLife" (640, 500) (10, 10)
 
 main :: IO ()
 main =
-  simulate window backgroundColour 30 initialGame gameAsPicture (\_ _ -> step)
+  simulate window backgroundColour 30 testBoard gameAsPicture (\_ _ -> step)

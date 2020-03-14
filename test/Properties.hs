92015ba17603f95ceb5bc2142596327f37dd5292
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-14 10:46:52 +0100

    Add unit test template

---
+++ test/Properties.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Test.Hspec
+
+main :: IO ()
+main =
+  hspec $ do
+    describe "Prelude.head" $ do
+      it "returns the first element of a list" $ do
+        head [23 ..] `shouldBe` (23 :: Int)

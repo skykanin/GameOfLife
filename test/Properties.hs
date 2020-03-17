54083da50391e9ef434c3e6e71af43dcd3740d70
skykanin <3789764+skykanin@users.noreply.github.com>
2020-03-17 20:43:29 +0100

    Add unit tests for index and helper functions

--- test/Properties.hs
+++ test/Properties.hs
@@ -1,10 +1,25 @@
 module Main where
 
+import Game (findIndex, helper)
 import Test.Hspec
 
 main :: IO ()
 main =
   hspec $ do
-    describe "Prelude.head" $ do
-      it "returns the first element of a list" $ do
-        head [23 ..] `shouldBe` (23 :: Int)
+    describe "Index function" $ do
+      it "returns the correct index given w, x and y" $ do
+        findIndex 20 0 0 `shouldBe` 0
+        findIndex 20 3 3 `shouldBe` 3 + 20 * 3
+        findIndex 5 3 4 `shouldBe` 3 + 5 * 4
+        findIndex 8 15 10 `shouldBe` 15 + 8 * 10
+    describe "Index helper function" $ do
+      it "returns the same as findIndex for happy path" $ do
+        helper 20 0 0 (succ, succ) `shouldBe` Just (findIndex 20 (succ 0) (succ 0))
+        helper 10 3 5 (succ, pred) `shouldBe` Just (findIndex 10 (succ 3) (pred 5))
+        helper 20 4 4 (pred, succ) `shouldBe` Just (findIndex 20 (pred 4) (succ 4))
+        helper 20 1 1 (pred, pred) `shouldBe` Just (findIndex 20 (pred 1) (pred 1))
+      it "returns Nothing in case of negative values" $ do
+        helper 20 0 0 (succ, pred) `shouldBe` Nothing
+        helper 11 0 1 (pred, pred) `shouldBe` Nothing
+        helper 30 2 0 (succ, pred) `shouldBe` Nothing
+        helper 3 1 (-2) (succ, succ) `shouldBe` Nothing
